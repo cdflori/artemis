6733e1817d25ae94583f22d472c7312bbf7f24a7
Slogreco <shoshwright@hotmail.com>
2020-12-08 19:39:24 -0700

    finished adding compression

--- server.js
+++ server.js
@@ -34,6 +34,7 @@ app.use(
 );
 app.use(passport.initialize());
 app.use(passport.session());
+app.use(compression());
 
 // Set up handlebars
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
